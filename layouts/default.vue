dbc7029dcbcd6a0959a1541e0072df2def3a575b
David Smith <davidsmith@itsandbox.net>
2020-06-16 16:56:43 -0400

    Add App Title Setting

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,24 @@
     <nuxt />
   </div>
 </template>
-
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Hello World!'
+      }
+    },
+    head () {
+      return {
+        title: process.env.APP_TITLE,
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          // { hid: 'description', name: 'description', content: 'My custom description' }
+        ]
+      }
+    }
+  }
+</script>
 <style>
 
 </style>
