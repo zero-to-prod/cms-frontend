65c375c9916f0504b5b5d0c22870d349fbe5d1c7
David Smith <davidsmith@itsandbox.net>
2020-06-29 08:45:35 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,8 @@ export default {
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-    '@nuxtjs/auth'
+    '@nuxtjs/auth',
+    'nuxt-purgecss'
   ],
   router: {
     middleware: ['auth']
