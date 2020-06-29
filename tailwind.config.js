588c8412e496d26963c439421b7214f9688c00b0
David Smith <davidsmith@itsandbox.net>
2020-06-29 12:04:53 -0400

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,24 @@
 */
 module.exports = {
   theme: {
-    colors: {
-      'brand-primary': 'var(--color-brand-primary)',
-      'brand-secondary': 'var(--color-brand-secondary)',
-      'background-primary': 'var(--color-background-primary)',
-      'background-secondary': 'var(--color-background-secondary)',
-      'text-primary': 'var(--color-text-primary)',
-      'text-secondary': 'var(--color-text-secondary)',
-      'button-primary': 'var(--color-button-primary)',
-      'button-primary-hover': 'var(--color-button-primary-hover)',
-      'button-secondary': 'var(--color-button-secondary)',
-      'button-secondary-hover': 'var(--color-button-secondary-hover)'
-    }
+    // colors: {
+    //   'brand-primary': 'var(--color-brand-primary)',
+    //   'brand-secondary': 'var(--color-brand-secondary)',
+    //   'background-primary': 'var(--color-background-primary)',
+    //   'background-secondary': 'var(--color-background-secondary)',
+    //   'text-primary': 'var(--color-text-primary)',
+    //   'text-secondary': 'var(--color-text-secondary)',
+    //   'button-primary': 'var(--color-button-primary)',
+    //   'button-primary-hover': 'var(--color-button-primary-hover)',
+    //   'button-secondary': 'var(--color-button-secondary)',
+    //   'button-secondary-hover': 'var(--color-button-secondary-hover)'
+    // }
   },
   variants: {},
   plugins: [],
-  purge: {
-    enabled: true,
-    content: [
-      './dist/**/*.html',
-      './dist/**/*.vue',
-      './dist/**/*.jsx'
-    ]
-  }
+  purge: [
+    './dist/**/*.html',
+    './dist/**/*.vue',
+    './dist/**/*.jsx'
+  ]
 }
