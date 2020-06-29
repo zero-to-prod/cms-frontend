ee32f030cc5a6bc6503a70a04dfc4be3bd3af307
David Smith <davidsmith@itsandbox.net>
2020-06-29 09:28:59 -0400

    Add height to icons.

--- components/Icons/TemplateIcon.vue
+++ components/Icons/TemplateIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="icon h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+  <svg class="icon" viewBox="0 0 20 20" fill="currentColor">
     <path
       d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path>
   </svg>
@@ -15,6 +15,7 @@
 
 <style scoped>
   .icon {
+    height: 1.5rem;
     color: var(--color-icon-primary)
   }
 </style>
--- components/Icons/UsersIcon.vue
+++ components/Icons/UsersIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="icon h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+  <svg class="icon" viewBox="0 0 20 20" fill="currentColor">
     <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
   </svg>
 
@@ -14,6 +14,7 @@
 
 <style scoped>
   .icon {
+    height: 1.5rem;
     color: var(--color-icon-primary)
   }
 </style>
