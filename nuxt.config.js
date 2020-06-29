6bd9ad4f811f1e3f3f348ee114ebe5a0f8e798a8
D.A.Smith <dsmith@realclearpolitics.com>
2020-06-29 08:09:52 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   css: [
     '~/assets/css/spinkit.min.css',
     process.env.APP_THEME,
-    '~/assets/css/master.css',
+    // '~/assets/css/master.css',
   ],
   /*
   ** Plugins to load before mounting the App
@@ -111,7 +111,7 @@ export default {
   },
   pwa: {
     workbox: {
-      // clientsClaim: false
+      clientsClaim: false
     }
   }
 }
