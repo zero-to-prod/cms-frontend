9d4e61b69214c6ac5ee52f9efca26e8db100907b
David Smith <davidsmith@itsandbox.net>
2020-07-02 11:33:40 -0400

    Update Components.

--- plugins/master/vueMaterial.js
+++ plugins/master/vueMaterial.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
-import VueMaterial from 'vue-material'
+import {
+  MdApp,
+  MdDrawer,
+  MdToolbar,
+  MdButton,
+  MdList,
+  MdContent
+} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default-dark.css'
 import Menu from 'vue-material-design-icons/Menu.vue'
@@ -9,11 +16,16 @@ import ViewDashboard from 'vue-material-design-icons/ViewDashboard.vue'
 import Login from 'vue-material-design-icons/Login.vue'
 import Logout from 'vue-material-design-icons/Logout.vue'
 
+Vue.use(MdApp)
+Vue.use(MdDrawer)
+Vue.use(MdToolbar)
+Vue.use(MdButton)
+Vue.use(MdList)
+Vue.use(MdContent)
 Vue.component('md-menu', Menu)
 Vue.component('md-account-multiple', AccountMultiple)
 Vue.component('md-chevron-left', ChevronLeft)
 Vue.component('md-view-dashboard', ViewDashboard)
 Vue.component('md-login', Login)
 Vue.component('md-logout', Logout)
-Vue.use(VueMaterial)
 
